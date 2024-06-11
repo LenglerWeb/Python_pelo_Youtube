c5b05e25e2a80248d5f70e1ad42ef5bfc5930a29
Eduardo Lengler <42681048+LenglerWeb@users.noreply.github.com>
2024-06-11 17:02:51 -0300

    Aula 03 do Otávio Luis Miranda

---
+++ aula03.py
@@ -0,0 +1,5 @@
+nome = input("Informe o seu nome: ")
+print(f'O nome informado foi: {nome} e o tipo da variável é: {type(nome)}')
+
+idade = int(input("Informe a sua idade: "))
+print(f'{nome}, você nasceu em {2024-idade} e o tipo desta variável é: {type(idade)}')
