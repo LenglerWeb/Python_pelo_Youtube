31660efe8c8cbcad156a60402c1dd9fcaed1f452
Eduardo Lengler <42681048+LenglerWeb@users.noreply.github.com>
2024-06-11 17:03:53 -0300

    Aula05 do Otávio Luis Miranda

---
+++ aula05.py
@@ -0,0 +1,15 @@
+n1 = 10
+n2 = 3
+n3 = 2024
+
+divisao = n1 / n2
+print('{}'.format(divisao))
+print('{:.2f}'.format(divisao))
+print(f'{divisao:.2f}')
+
+print(f'{n1:0>10}')
+print(f'{n3:0>10.2f}')
+
+nome = "Eduardo Lengler"
+print(len(nome))
+print(f'{nome:*^50}')
